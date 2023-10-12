d34e765cc365ed965950afb9b92bc46d27ffb418
Rick Fisher <fshtank@users.noreply.github.com>
2023-10-11 22:01:08 -0700

    Spring Boot Initalizer Code (#2)

---
+++ src/test/java/com/fshtank/bls/BlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fshtank.bls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
