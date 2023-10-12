97fa414ac933c5d1be6e2b521c03a2317fe29903
Rick Fisher <fshtank@gmail.com>
2023-10-11 21:54:13 -0700

    Spring Boot Initalizer Code

---
+++ src/main/java/com/fshtank/bls/BlsApplication.java
@@ -0,0 +1,13 @@
+package com.fshtank.bls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlsApplication.class, args);
+	}
+
+}
